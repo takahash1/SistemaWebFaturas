2d968d91cffef93a670807f1dc9e331661483ede
takahash1 <45808212+takahash1@users.noreply.github.com>
2020-05-25 10:45:13 -0300

    Add files via upload

--- websystem/cadastrar.php
+++ websystem/cadastrar.php
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+<?php
+	require_once 'classes/usuarios.php';
+	$u = new Usuario;
+?>
+
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8">
@@ -6,38 +10,66 @@
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 	<title>Controle de faturas</title>
 	<link rel="stylesheet" href="css/style.css">
-	<script type="text/javascript" src="js/script.js"></script>
 </head>
 <body>
-	<div id="fb-root"></div>
-	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v7.0&appId=249156916192097&autoLogAppEvents=1"></script>
-	<script>
-	  	window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '{249156916192097}',
-	      cookie     : true,
-	      xfbml      : true,
-	      version    : '{v7.0}'
-	    });
-	      
-	    FB.AppEvents.logPageView();   
-	 	 };
-	</script>
 
 	<div id="formulario">
 	<h1>Insira seus dados</h1>	
 	<h2>e realize seu cadastro</h2>
-	<form method="POST" action="process.php">
-		<input type="text" minlength="3" placeholder="Digite seu nome" required="">
-		<input type="text" minlength="3" placeholder="Digite seu sobrenome" required="">
-		<input type="tel" required="required" maxlength="15" name="phone" pattern="(\([0-9]{2}\))\s([9]{1})?([0-9]{4})-([0-9]{4})" title="Número de telefone precisa ser no formato (99) 9999-9999" placeholder="Digite seu telefone" />
-		<input type="email" placeholder="Digite seu e-mail">
-		<input type="email" placeholder="Confirme seu e-mail">
-		<input type="password" placeholder="Digite sua senha">
-		<input type="password" placeholder="Confirme sua senha">
-		<div class="fb-login-button" id="fb" data-size="large" data-button-type="continue_with" data-layout="rounded" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>
+	<form method="POST">
+		<input type="text" name="nome"  maxlength="30" placeholder="Digite seu nome completo"> 
+		<input type="tel" name="telefone" maxlength="30" name="phone" title="Número de telefone precisa ser no formato (99) 9999-9999" placeholder="Digite seu telefone" />
+		<input type="email" name="email" placeholder="Digite seu e-mail" maxlength="40">
+		<input type="email" name="confEmail" placeholder="Confirme seu e-mail" maxlength="40">
+		<input type="password" name="senha" placeholder="Digite sua senha" maxlength="15">
+		<input type="password" name="confSenha" placeholder="Confirme sua senha" maxlength="15">
 		<input type="submit" value="Cadastrar">	
 	</form>
 	</div>
+
+<?php
+
+if(isset($_POST['nome']))
+{
+	$nome = addslashes($_POST['nome']);
+	$telefone = addslashes($_POST['telefone']);
+	$email = addslashes($_POST['email']);
+	$confirmarEmail = addslashes($_POST['confEmail']);
+	$senha = addslashes($_POST['senha']);
+	$confirmarSenha = addslashes($_POST['confSenha']);
+
+	if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($confEmail) && !empty($senha) && !empty($confSenha))
+	{
+		$u->conectar("faturas", "localhost", "root", "");
+		if($u->msgErro == "")
+		{
+			if($senha == $confirmarSenha)
+			{
+				if($u->cadastrar($nome, $telefone, $email, $senha))
+				{
+					echo "Cadastrado com sucesso";
+				}
+				else
+				{
+					echo "Email ja cadastrado";
+				}
+			}
+			else
+			{
+				echo "Senha e confirmar senha não conferem!";
+			}
+		}
+		else
+		{
+			echo "Erro: ".$u->msgErro;
+		}
+	}
+	else
+	{
+		echo "Preencha todos os campos!"; 
+	}	
+}
+
+?>
 </body>
 </html>
